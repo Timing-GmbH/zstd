4f8e9aa7b5ca8f4dbe4b0603c59ffcb39626e084
Daniel Alm <Daniel.Alm@ForumD.net>
2021-01-29 13:03:11 +0100

    Another attempt at fixing the package structure.

--- Package.swift
+++ Package.swift
@@ -3,18 +3,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "zstd",
-    products: [
-        .library(
-            name: "zstd",
-            targets: ["zstd"]),
-    ],
-    targets: [
-        .target(
-            name: "zstd",
+	name: "zstd",
+	products: [
+		.library(
+			name: "zstd",
+			targets: ["zstd"]),
+	],
+	targets: [
+		.target(
+			name: "zstd",
 			path: "lib",
 			exclude: ["dll"],
-			publicHeadersPath: "lib"),
-    ],
+			publicHeadersPath: "."),
+	],
 	cLanguageStandard: .gnu11
 )
